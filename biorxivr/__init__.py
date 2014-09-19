15c2b7fa91eec10b68fdf5aa6c6439bf81c5dc38
Jake Johnson <jake@archive.org>
2014-09-19 14:00:00 -0700

    init

---
+++ biorxivr/__init__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+bioRxiv Archiver
+~~~~~~~~~~~~~~~~
+
+:copyright: (c) 2014 Internet Archive
+:license: AGPL 3, see LICENSE for more details.
+"""
+
+__title__ = 'archive_biorxiv'
+__version__ = '0.0.1'
+__author__ = 'Jake Johnson'
+__license__ = 'AGPL 3'
+__copyright__ = 'Copyright 2014 Internet Archive'
